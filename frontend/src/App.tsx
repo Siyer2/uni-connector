33072e342466cf2aa9f8e8235bf733ee78f68b16
Victor Leng <z5421261@unsw.edu.au>
2022-09-22 14:05:15 +1000

    Add Victor to website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload. A website by Syam
+          victor
         </p>
         <a
           className="App-link"
