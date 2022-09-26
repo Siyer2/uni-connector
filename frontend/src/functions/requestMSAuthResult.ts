9b6fc66809c5c5237991dcfcca00a22e3baf615e
Syam Iyer <iyersyam@gmail.com>
2022-09-26 12:29:58 +1000

    Add requestMSAuthResult func

---
+++ frontend/src/functions/requestMSAuthResult.ts
@@ -0,0 +1,21 @@
+import { AccountInfo, IPublicClientApplication } from '@azure/msal-browser';
+import { loginRequest } from '../authConfig';
+
+export async function requestMSAuthResult(
+  instance: IPublicClientApplication,
+  account: AccountInfo
+) {
+  const request = {
+    ...loginRequest,
+    account,
+  };
+
+  // Silently acquires an access token which is then attached to a request for Microsoft Graph data
+  try {
+    const response = await instance.acquireTokenSilent(request);
+    return response;
+  } catch (error) {
+    console.log('failed to login', error);
+    throw new Error('failed to login');
+  }
+}
