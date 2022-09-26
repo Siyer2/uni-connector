11fccec67dcb291e7c6304ad1e2f6038b037f2d6
Syam Iyer <iyersyam@gmail.com>
2022-09-26 12:29:07 +1000

    Add auth config

---
+++ frontend/src/authConfig.ts
@@ -0,0 +1,23 @@
+import { Configuration } from '@azure/msal-browser';
+
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: process.env.REACT_APP_MS_CLIENT_ID!,
+    authority: 'https://login.microsoftonline.com/common',
+    redirectUri: process.env.REACT_APP_MS_REDIRECT_URI,
+  },
+  cache: {
+    cacheLocation: 'sessionStorage', // This configures where your cache will be stored
+    storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+  },
+};
+
+// Add scopes here for ID token to be used at Microsoft identity platform endpoints.
+export const loginRequest = {
+  scopes: ['User.Read'],
+};
+
+// Add the endpoints here for Microsoft Graph API services you'd like to use.
+export const graphConfig = {
+  graphMeEndpoint: 'https://graph.microsoft.com/v1.0/me',
+};
