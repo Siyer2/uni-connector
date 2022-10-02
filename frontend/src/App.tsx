f4c85bacf6df6dc3635c9646670fab5aa6ce1068
Jennifer <jennifer.sun189@gmail.com>
2022-10-02 20:57:39 +1100

    Add Jennifer to website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload. A website by Syam,
-          Victor, Muhammed, Vivian, Derek!
+          Victor, Muhammed, Vivian, Derek, Jennifer!
         </p>
         <a
           className="App-link"
