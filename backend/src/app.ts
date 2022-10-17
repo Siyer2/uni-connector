0d86dd5202e274a3236a909cb89a0102771ecc8f
Syam Iyer <iyersyam@gmail.com>
2022-10-18 00:03:50 +1100

    Fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,16 @@
 import * as express from 'express';
 import { authenticateUser } from './middleware/authenticateUser';
+import * as cors from 'cors';
 
 const app = express();
+
+// CORS
+const allowlist = ['http://localhost:3001'];
+const options: cors.CorsOptions = {
+  origin: allowlist,
+};
+app.use(cors(options));
+app.use(express.json());
 
 // Routes
 app.get('/', authenticateUser, (req, res) => {
