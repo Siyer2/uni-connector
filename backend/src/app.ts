db448e0fd6ba7f78fa3abee7d9bc236d593359b3
Syam Iyer <iyersyam@gmail.com>
2022-10-18 09:42:24 +1100

    Add env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
 import * as express from 'express';
 import { authenticateUser } from './middleware/authenticateUser';
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 
+dotenv.config({ path: __dirname + '/../.env.local' });
 const app = express();
 
 // CORS
-const allowlist = ['http://localhost:3001'];
+const allowlist =
+  process.env.ENVIRONMENT === 'local'
+    ? ['http://localhost:3001']
+    : ['https://main.d2dflf7eo6e40g.amplifyapp.com/'];
 const options: cors.CorsOptions = {
   origin: allowlist,
 };
@@ -14,6 +19,8 @@ app.use(express.json());
 
 // Routes
 app.get('/', authenticateUser, (req, res) => {
+  console.log(__dirname);
+  console.log(process.env.ENVIRONMENT);
   console.log(req.user);
   res.send(`Request received: ${req.method} - ${req.path}`);
 });
