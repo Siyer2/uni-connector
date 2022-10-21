1dc383b5d246fe9840bcef1000c2e0890e57ed9f
Victor Leng <z5421261@unsw.edu.au>
2022-10-21 20:38:55 +1100

    establishing general structure of update user page

--- frontend/src/pages/UpdateUser.tsx
+++ frontend/src/pages/UpdateUser.tsx
@@ -1,5 +1,55 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import { ThemeProvider } from '@mui/system';
+import { theme } from '../theme';
 
 export const UpdateUser = () => {
-  return <Box>This is the UpdateUser Page!</Box>;
+  return (
+    <ThemeProvider theme={theme}>
+      <Box
+        sx={{
+          bgcolor: theme.palette.background.default,
+          color: theme.palette.primary.contrastText,
+          height: '100vh',
+        }}
+      >
+        <Typography variant="h1" textAlign={'center'}>
+          Let's get to know you!
+        </Typography>
+        <Box
+          sx={{
+            fontSize: 30,
+            textAlign: 'left',
+            fontWeight: 'medium',
+            mx: '70vh',
+            py: '10vh',
+          }}
+        >
+          <Box>What faculty are you in?</Box>
+          <Box
+            sx={{
+              mb: '150px',
+            }}
+          >
+            &lt;dropdownbox&gt;
+          </Box>
+          <Box>I'm into...</Box>
+          <Box
+            sx={{
+              mb: '150px',
+            }}
+          >
+            &lt;textbox&gt;
+          </Box>
+          <Box
+            sx={{
+              fontSize: 30,
+              textAlign: 'center',
+            }}
+          >
+            &lt;submit&gt;
+          </Box>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
 };
