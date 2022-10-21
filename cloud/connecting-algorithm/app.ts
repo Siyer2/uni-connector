f94778717e21ee7be5bc856b3262f28aa4b790e5
Syam Iyer <iyersyam@gmail.com>
2022-10-21 16:43:58 +1100

    Ensure local dev works

--- cloud/connecting-algorithm/app.ts
+++ cloud/connecting-algorithm/app.ts
@@ -1,4 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import DynamoDB from 'aws-sdk/clients/dynamodb';
+import * as dotenv from 'dotenv';
+dotenv.config({ path: __dirname + '/../.env.local' });
+
+const docConfig = {
+  region: 'us-east-1',
+  endpoint: 'http://localhost:8002',
+  credentials: {
+    accessKeyId: 'localKey',
+    secretAccessKey: 'localSecret',
+  },
+};
+const docClient =
+  process.env.ENVIRONMENT === 'local'
+    ? new DynamoDB.DocumentClient(docConfig)
+    : new DynamoDB.DocumentClient();
 
 /**
  *
@@ -10,25 +26,27 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
-export const lambdaHandler = async (): Promise<APIGatewayProxyResult> => {
-  console.log('here');
-  let response: APIGatewayProxyResult;
-  try {
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: 'hello world',
-      }),
-    };
-  } catch (err: unknown) {
-    console.log(err);
-    response = {
-      statusCode: 500,
-      body: JSON.stringify({
-        message: err instanceof Error ? err.message : 'some error happened',
-      }),
-    };
-  }
+async function getUsers() {
+  const params = {
+    TableName: 'User',
+    KeyConditionExpression: 'id = :value',
+    ExpressionAttributeValues: {
+      ':value': '1',
+    },
+  };
 
-  return response;
+  const userInDb = await docClient.query(params).promise();
+  return userInDb;
+}
+
+export const lambdaHandler = async (): Promise<APIGatewayProxyResult> => {
+  const users = await getUsers();
+  console.log(users);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'hello world',
+    }),
+  };
 };
