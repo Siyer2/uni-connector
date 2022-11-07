bb7e31d04a13647aaa14ec4d639c48c2ebbb2629
Syam Iyer <iyersyam@gmail.com>
2022-11-07 12:22:43 +1100

    Add type for user in header

--- backend/src/middleware/types.ts
+++ backend/src/middleware/types.ts
@@ -11,10 +11,26 @@ export interface Key {
   issuer: 'https://login.microsoftonline.com/{tenantid}/v2.0';
 }
 
+export type MicrosoftUser = {
+  iss: string;
+  iat: number;
+  nbf: number;
+  exp: number;
+  name: string;
+  nonce: string;
+  oid: string;
+  preferred_username: string;
+  rh: string;
+  sub: string;
+  tid: string;
+  uti: string;
+  ver: string;
+};
+
 declare global {
   namespace Express {
     interface Request {
-      user: any; //TODO: Add user type
+      user: MicrosoftUser;
       db: DynamoDB.DocumentClient;
     }
   }
