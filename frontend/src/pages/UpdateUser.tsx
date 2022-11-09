54c5c9a4697014cf92fd7f4dd4ad648cdb120c67
Victor Leng <z5421261@unsw.edu.au>
2022-11-09 12:45:17 +1100

    added user input to UpdateUser.tsx (does not do anything with input)

--- frontend/src/pages/UpdateUser.tsx
+++ frontend/src/pages/UpdateUser.tsx
@@ -1,6 +1,19 @@
-import { Box, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { ThemeProvider } from '@mui/system';
 import { theme } from '../theme';
+
+// Functionality and then appearance
+
+// TODO: change on select
+let facultyId: number = -1;
 
 export const UpdateUser = () => {
   return (
@@ -10,45 +23,42 @@ export const UpdateUser = () => {
           bgcolor: theme.palette.background.default,
           color: theme.palette.primary.contrastText,
           height: '100vh',
+          textAlign: 'center',
         }}
       >
-        <Typography variant="h1" textAlign={'center'}>
-          Let's get to know you!
-        </Typography>
-        <Box
+        <Typography variant="h1">Let's get to know you!</Typography>
+        <Grid
+          container
+          spacing={8}
           sx={{
             fontSize: 30,
-            textAlign: 'left',
+            flexDirection: 'column',
             fontWeight: 'medium',
-            mx: '70vh',
-            py: '10vh',
           }}
         >
-          <Box>What faculty are you in?</Box>
-          <Box
-            sx={{
-              mb: '150px',
-            }}
-          >
-            &lt;dropdownbox&gt;
-          </Box>
-          <Box>I'm into...</Box>
-          <Box
-            sx={{
-              mb: '150px',
-            }}
-          >
-            &lt;textbox&gt;
-          </Box>
-          <Box
-            sx={{
-              fontSize: 30,
-              textAlign: 'center',
-            }}
-          >
-            &lt;submit&gt;
-          </Box>
-        </Box>
+          {/* TODO: Make fields required */}
+          <Grid item>
+            {/* Change facultyId on event */}
+            <Box>What faculty are you in?</Box>
+            <Select label="Age">
+              <MenuItem value={0}>Arts, Design & Architecture</MenuItem>
+              <MenuItem value={1}>Business</MenuItem>
+              <MenuItem value={2}>Engineering</MenuItem>
+              <MenuItem value={3}>Law</MenuItem>
+              <MenuItem value={4}>Medicine</MenuItem>
+              <MenuItem value={5}>Science</MenuItem>
+            </Select>
+          </Grid>
+          <Grid item>
+            <Box>I'm into...</Box>
+            {/* TODO: Take information from input*/}
+            <TextField label="Write something" />
+          </Grid>
+          <Grid item>
+            {/* TODO: Submit inputted information */}
+            <Button variant="contained">Submit</Button>
+          </Grid>
+        </Grid>
       </Box>
     </ThemeProvider>
   );
