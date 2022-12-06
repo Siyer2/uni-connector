6d65fd1ca37b6a59758b257ea1c45aae9d68ac72
Syam Iyer <iyersyam@gmail.com>
2022-12-06 19:36:28 +1100

    Update types

--- backend/src/user/types.ts
+++ backend/src/user/types.ts
@@ -1,9 +1,13 @@
-export type User = {
-  id: string;
+interface TuesHeyDBEntry {
+  primaryKey: string;
+  sortKey: string;
+}
+
+export interface User extends TuesHeyDBEntry {
   faculty: Faculty;
   imInto: string;
   [key: string]: string;
-};
+}
 
 export enum Faculty {
   ArtsDesignAndArchitecture = 'artsDesignAndArchitecture',
