e335bd0f1e836263dc46ae249b455395fba43b4c
Syam Iyer <iyersyam@gmail.com>
2023-01-08 17:41:31 +1100

    Fix typo

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL:
     process.env.REACT_APP_ENVIRONMENT === 'local'
       ? 'http://localhost:3000'
-      : ' https://dvf1x3tv49.execute-api.ap-southeast-2.amazonaws.com',
+      : 'https://dvf1x3tv49.execute-api.ap-southeast-2.amazonaws.com',
   headers: {
     'Content-type': 'application/json',
   },
