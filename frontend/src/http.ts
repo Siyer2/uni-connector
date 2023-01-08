7a1db5b3e1a668a8024e67d7df3c0e1a9603acb8
Syam Iyer <iyersyam@gmail.com>
2023-01-08 17:45:18 +1100

    Fix typo in environment variables

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   baseURL:
-    process.env.REACT_APP_ENVIRONMENT === 'local'
+    process.env.REACT_APP_DEPLOYMENT === 'local'
       ? 'http://localhost:3000'
       : 'https://dvf1x3tv49.execute-api.ap-southeast-2.amazonaws.com',
   headers: {
