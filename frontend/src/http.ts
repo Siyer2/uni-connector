b89b9c171823ca0ef61b9278b07169a1a873a5c7
Syam Iyer <iyersyam@gmail.com>
2023-01-10 23:34:22 +1100

    Update http.ts

--- frontend/src/http.ts
+++ frontend/src/http.ts
@@ -4,7 +4,7 @@ export default axios.create({
   baseURL:
     process.env.REACT_APP_DEPLOYMENT === 'local'
       ? 'http://localhost:3000'
-      : 'https://dvf1x3tv49.execute-api.ap-southeast-2.amazonaws.com',
+      : 'https://7jc6jqtf51.execute-api.ap-southeast-2.amazonaws.com',
   headers: {
     'Content-type': 'application/json',
   },
