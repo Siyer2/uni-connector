ce6e7513827a11d2bbd22cfdb7cb746befd1bdb8
kumsuzCode <mkumsuz1@gmail.com>
2023-01-12 10:33:18 +1100

    Make request to backend using updateUser function after user fills out form on update-user page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -45,7 +45,6 @@ export const HomePage = () => {
           setOpen(true);
         }
       }
-
     }
 
     if (isAuthenticated) {
--- frontend/src/pages/UpdateUser.tsx
+++ frontend/src/pages/UpdateUser.tsx
@@ -11,6 +11,9 @@ import {
 import TopAppBar from '../components/TopAppBar';
 import { useState } from 'react';
 import { Faculty } from '../types';
+import { updateUser } from '../api';
+import { useNavigate } from 'react-router-dom';
+import { getToken } from '../localStorage';
 
 export const UpdateUser = () => {
   const [userDetails, setUserDetails] = useState<{
@@ -19,6 +22,8 @@ export const UpdateUser = () => {
     faveEat: string;
     interests: string;
   }>({ emojis: '', faculty: Faculty.Business, faveEat: '', interests: '' });
+
+  const navigate = useNavigate();
 
   // TODO:
   // Add appropriate type to event
@@ -32,92 +37,96 @@ export const UpdateUser = () => {
 
   const handleSubmit = async () => {
     console.log(userDetails);
-    // TODO:
-    // Hit the correct API to successfully store info to the db
-    const res = await fetch('http://localhost:3001/updateUser', {
-      method: 'POST',
-    });
-    await res.json();
+    try {
+      const token = getToken();
+      const user = await updateUser(token, userDetails);
+      console.log(user);
+      navigate('/chats');
+    } catch (err: any) {
+      if (err.response) {
+        console.log(err.response.data);
+      } else {
+        console.log(err.message);
+      }
+    }
   };
 
   return (
-      <TopAppBar>
-        <Grid
-          item
-          xs={12}
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <Typography variant={'h5'}>This is...</Typography>
-          <Typography variant={'h2'}>TuesHey</Typography>
-          <Typography variant={'h4'}>Let's get to know you</Typography>
-          <Typography variant={'h4'}>✌️😙✌️</Typography>
+    <TopAppBar>
+      <Grid
+        item
+        xs={12}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant={'h5'}>This is...</Typography>
+        <Typography variant={'h2'}>TuesHey</Typography>
+        <Typography variant={'h4'}>Let's get to know you</Typography>
+        <Typography variant={'h4'}>✌️😙✌️</Typography>
+      </Grid>
+      <Grid item container xs={12} paddingX={5} style={{ gap: 15 }}>
+        <Grid item xs={12}>
+          <TextField
+            onChange={handleChange}
+            value={userDetails.emojis}
+            placeholder={'😎 ⚽ 🍔'}
+            name="emojis"
+            label={'Describe yourself in 3 emojis!'}
+            fullWidth
+          />
         </Grid>
-        <Grid item container xs={12} paddingX={5} style={{ gap: 15 }}>
-          <Grid item xs={12}>
-            <TextField
+        <Grid item xs={12}>
+          <FormControl fullWidth required>
+            <InputLabel>Choose your faculty</InputLabel>
+            <Select
+              value={userDetails.faculty}
+              label="Choose your faculty"
               onChange={handleChange}
-              value={userDetails.emojis}
-              placeholder={'😎 ⚽ 🍔'}
-              name="emojis"
-              label={'Describe yourself in 3 emojis!'}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl fullWidth required>
-              <InputLabel>Choose your faculty</InputLabel>
-              <Select
-                value={userDetails.faculty}
-                label="Choose your faculty"
-                onChange={handleChange}
-                name="faculty"
-              >
-                <MenuItem value={Faculty.ArtsDesignAndArchitecture}>
-                  Arts, Design & Architecture
-                </MenuItem>
-                <MenuItem value={Faculty.Business}>Business School</MenuItem>
-                <MenuItem value={Faculty.Engineering}>Engineering</MenuItem>
-                <MenuItem value={Faculty.LawAndJustice}>
-                  Law and Justice
-                </MenuItem>
-                <MenuItem value={Faculty.MedicineAndHealth}>
-                  Medicine and Health
-                </MenuItem>
-                <MenuItem value={Faculty.Science}>Science</MenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label={"What's your favourite place to eat on campus?"}
-              value={userDetails.faveEat}
-              onChange={handleChange}
-              name="bestEat"
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              label={"I'm into..."}
-              value={userDetails.interests}
-              placeholder={'hip-hop, Simpsons'}
-              onChange={handleChange}
-              name="interests"
-              fullWidth
-            />
-          </Grid>
+              name="faculty"
+            >
+              <MenuItem value={Faculty.ArtsDesignAndArchitecture}>
+                Arts, Design & Architecture
+              </MenuItem>
+              <MenuItem value={Faculty.Business}>Business School</MenuItem>
+              <MenuItem value={Faculty.Engineering}>Engineering</MenuItem>
+              <MenuItem value={Faculty.LawAndJustice}>Law and Justice</MenuItem>
+              <MenuItem value={Faculty.MedicineAndHealth}>
+                Medicine and Health
+              </MenuItem>
+              <MenuItem value={Faculty.Science}>Science</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label={"What's your favourite place to eat on campus?"}
+            value={userDetails.faveEat}
+            onChange={handleChange}
+            name="faveEat"
+            fullWidth
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            label={"I'm into..."}
+            value={userDetails.interests}
+            placeholder={'hip-hop, Simpsons'}
+            onChange={handleChange}
+            name="interests"
+            fullWidth
+          />
+        </Grid>
 
-          <Grid item xs={12}>
-            <Button variant={'contained'} onClick={handleSubmit}>
-              Submit!
-            </Button>
-          </Grid>
+        <Grid item xs={12}>
+          <Button variant={'contained'} onClick={handleSubmit}>
+            Submit!
+          </Button>
         </Grid>
-      </TopAppBar>
+      </Grid>
+    </TopAppBar>
   );
 };
