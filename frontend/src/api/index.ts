e1ebd890b5a5c3c21c365fa73719294a750db1ae
kumsuzCode <mkumsuz1@gmail.com>
2023-01-12 09:51:58 +1100

    Created updateUser function to make request to backend

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -10,3 +10,13 @@ export async function loginUser(token: string): Promise<User> {
   const user = res.data as User;
   return user;
 }
+
+export async function updateUser(token: string, userInfo: any): Promise<User> {
+  const res = await http.post('/updateUser', userInfo, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const user = res.data as User;
+  return user;
+}
