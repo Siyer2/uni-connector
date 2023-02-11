0826f21d2357a7b7a0273360fd88f8f3f0e0bbd4
derekxu04 <39829620+derekxu04@users.noreply.github.com>
2023-02-11 12:05:33 +1100

    style

--- backend/src/chats/index.ts
+++ backend/src/chats/index.ts
@@ -1,12 +1,12 @@
 import { StreamChat } from 'stream-chat';
 
 export function getChatUserToken(user_id: string): string {
-  // Define values.
-  const api_key = process.env.REACT_APP_STREAM_API_KEY;
-  const api_secret = process.env.REACT_APP_STREAM_API_SECRET;
+  // Define values
+  const apiKey = process.env.REACT_APP_STREAM_API_KEY;
+  const apiSecret = process.env.REACT_APP_STREAM_API_SECRET;
 
   // Initialize a Server Client
-  const serverClient = StreamChat.getInstance(api_key, api_secret);
+  const serverClient = StreamChat.getInstance(apiKey, apiSecret);
   // Create User Token
   const token = serverClient.createToken(user_id);
 
