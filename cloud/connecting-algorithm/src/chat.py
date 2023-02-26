06464ca797c340d859905fad22dc3166291e475f
derekxu04 <39829620+derekxu04@users.noreply.github.com>
2023-02-26 21:57:01 +1100

    Fix create_channel in chat.py

--- cloud/connecting-algorithm/src/chat.py
+++ cloud/connecting-algorithm/src/chat.py
@@ -1,6 +1,6 @@
 # This file contains helper functions to use the stream chat client
 
-from stream_chat import StreamChat, Channel
+from stream_chat import StreamChat
 
 # Returns StreamChat chat client
 def get_chat_client():
@@ -12,11 +12,10 @@ def get_chat_client():
 
 # TODO: what if create channel between same two users
 # Creates channel between two users
-def create_channel(user1Id, user2Id):
-    channel = Channel("messaging", None, custom_data=dict(members=[
+def create_channel(client, user1Id, user2Id):
+    channel = client.channel("messaging", None, custom_data=dict(members=[
         user1Id[5:],    # to get rid of #USER prefix
         user2Id[5:]
     ]))
 
-    # Creates the channel
-    channel.query()
+    channel.create()
